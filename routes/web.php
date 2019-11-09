8037012378e141912d2c395aa7e270ea9580faf9
Amirul Faiz <amirulfaiz.work@gmail.com>
2019-11-09 11:43:21 +0800

    Second Commit

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testing', function () {
+    echo 'This is Testing';
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
